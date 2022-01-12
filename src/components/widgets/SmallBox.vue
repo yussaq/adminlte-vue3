4c272defa44c87f0bc024c396b861197659c9f6d
yussaq <yussaq.nf@gmail.com>
2022-01-12 14:09:05 +0700

    Boxes Components

---
+++ src/components/widgets/SmallBox.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="small-box" :class=bg>
+      <div class="inner">
+        <h3> {{ innerNumber }} </h3>
+        <p>{{ innerText }} </p>
+      </div>
+      <div class="icon">
+        <i class="fas fa-shopping-cart" :class=icon></i>
+      </div>
+      <a href="javascript:void(0)" class="small-box-footer">
+        More info <i class="fas fa-arrow-circle-right"></i>
+      </a>
+    </div>    
+</template>
+
+<script>
+export default {
+    props:{
+        bg:{
+            type:String,
+            default:'bg-info'
+        },
+        innerNumber:{
+            type:Number,
+            default:0
+        },
+        innerText:{
+            type:String,
+            default:'innerText'
+        },
+        icon:{
+            type:String,
+            default:'fas fa-shopping-cart'
+        }                
+
+    }
+}
+</script>
